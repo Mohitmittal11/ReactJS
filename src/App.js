88bb87632a5d3762ead636b2205567469879fe62
Mohit Mittal <you@example.com>
2024-03-04 22:44:16 +0530

    practice

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to React JS.</h1>
+      <p> This is another Paragraph</p>
+      <h2>This is a heading</h2>
     </div>
   )
 };
