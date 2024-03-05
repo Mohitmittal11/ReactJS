2db702250577795bad8410911099442518eb2dba
Mohit Mittal <mittalm904@gmail.com>
2024-03-05 10:17:49 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
       <h1>Welcome to React JS.</h1>
       <p> This is another Paragraph</p>
       <h2>This is a heading</h2>
+      <p>Hello</p>
+      <p> HELLO's</p>
     </div>
   )
 };
